741e6e77f5aa56bbf53219565eae79c4320b2879
Brahm V <brahmvanh@gmail.com>
2023-01-11 14:06:21 -0500

    Finalized javascript. Created README.

--- README.md
+++ README.md
@@ -1,46 +1,23 @@
-// Example of using API keys, uses openWeatherAPI, so we'll need most of this information
-// https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
+## Weather Forecast
 
-// Header in HTML >>
-// Search area in HTML >>
-// Landscape card containing current weather parameters for location >>
-// 5 vertical cards(maybe?) for next five days' forecast >>
-// Link to those font-fantastic mini images to use for search button, etc.  >>
+# Description
 
-// Current weather - display time weather was last updated
-// ***NEED TO DECIDE ON MORE WEATHER PARAMETERS TO INCLUDE
-// IMAGE OF WEATHER FOR DAY ON CARD
-//  High and low for day
-//  Current temp
-// feels like
-// wind direction and speed
-// Sky condition (cloudy, etc.)
+This application is designed to allow the user to search for any city in the world with weather data and retrieve a five-day weather forecast for that city.
 
 
-// 5 day-cards style: hmmmmmmmmmmmmmmmmmmmmmmmm
-// use five different colors for cards (then use that blue and white from skis as the rest of the page?)XXXX
-// Use cards with rounded corners, the cliche? >>
-// Use circles for card shapes? XXXX
-// Could get really against-grain - maybe use different sized cards for each day? XXX
-// CAN WE GET A LIVE-UPDATE RADAR?? XXX
-// Can we add an individual HTML page that is link to in the 5-day cards? Maybe it autofills one generic HTML with the day's full-detail forecast 
 
+# Installation
 
-// Make sure to add media-queries to allow use over multiple devices, as well as accessibility features for screen readers
-// NEED TO MAKE HTML PAGE DYNAMIC AND ADD CARDS WHEN USER SEARCHES FOR WEATHER.
+N/A
 
+# Usage
 
-// remove class main from jumbotron 
-// add class row to jumbotron
-// remove class display from navSection 
-// add display none to landing page 
-// remove class display from six day forecast
+This application is to be used for anyone who is looking for a five-day weather forecast online
 
+# Credits
 
-TO DO
+N/A
 
-// Make search require state information
-// Need to make a list of search history using local storage
-//
+# License
 
-
+Please refer to license in Repo.
--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,93 @@
+// Plugins to be used later in script
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
 
-const apiUrl = 'http://api.openweathermap.org'
-const apiKey = 'e943c609140455c43be229fc218f1f3a'
 
-var cityInputEl = document.querySelector('#searchInput');
-var formEl = document.querySelector('#searchForm');
-var searchBtn = document.querySelector('#searchBtn');
+const apiUrl = 'http://api.openweathermap.org';
+const apiKey = 'e943c609140455c43be229fc218f1f3a';
 
-  
-  
-function timeUpdate() {
-  
-  const currentDate = dayjs().format('MMM D, YYYY');
-  const currentTime = dayjs().format('h:mm:ss a');
-  document.querySelector('#currentDateTime').textContent = currentDate + ' ' + currentTime;
-}
-  
-setInterval(timeUpdate, 1000);
+// Variables to be used later in script
 
-function submitHandler(event) {
+var cityInputEl = $('#searchInput');
+var formEl = $('#searchForm');
+var searchBtn = $('#searchBtn');
+var cities = [];
+var date = dayjs().format('MMM D, YYYY');
+
+
+var date2 = $('#date2');
+var date3 = $('#date3');
+var date4 = $('#date4');
+var date5 = $('#date5');
+var date6 = $('#date6');
+
+var temp2 = $('#temp2');
+var temp3 = $('#temp3');
+var temp4 = $('#temp4');
+var temp5 = $('#temp5');
+var temp6 = $('#temp6');
+
+var windDirection2 = $('#windDirection2');
+var windDirection3 = $('#windDirection3');
+var windDirection4 = $('#windDirection4');
+var windDirection5 = $('#windDirection5');
+var windDirection6 = $('#windDirection6');
+
+var windSpeed2 = $('#windSpeed2');
+var windSpeed3 = $('#windSpeed3');
+var windSpeed4 = $('#windSpeed4');
+var windSpeed5 = $('#windSpeed5');
+var windSpeed6 = $('#windSpeed6');
+
+var humidity2 = $('#humidity2');
+var humidity3 = $('#humidity3');
+var humidity4 = $('#humidity4');
+var humidity5 = $('#humidity5');
+var humidity6 = $('#humidity6');
 
    
 
+// Function to update time live on the page
+
+function timeUpdate() {
+  const currentDate = dayjs().format('MMM D, YYYY');
+  const currentTime = dayjs().format('h:mm:ss a');
+  $('#currentDateTime').text(currentTime + ' on ' + currentDate);
+}
+
+setInterval(timeUpdate, 1000);
+
+// Used an if-statement to retrive local storage items on page 
+
+// Function to scan local storage for search history and populate the page with recent searches
+
+function retrieveHistory() {
+  var retrieveHistory = localStorage.getItem("Search Result");
+
+  if (retrieveHistory) {
+    cities = retrieveHistory.split(",");
+    renderButtons();
+  }
+}
+// Function to handle the submission of the user entry
+
+function submitSearch(event) {
     event.preventDefault();
-    var cityName = cityInputEl.value.trim();
+    moveNavSection();
+    clearResultsField;
+
+    var cityName = cityInputEl.val().trim();
     getLatLon(cityName);
     console.log('clicked');
 
-
 }
+
+// Function to retrieve latitude and longitude data about city in search and passes on to getGetCity info to retrieve the weather data
 
 function getLatLon(search) {
 
-  var url = apiUrl + '/geo/1.0/direct?q='+ search + '&limit=5&appid=' + apiKey;
+    var url = apiUrl + '/geo/1.0/direct?q='+ search + '&limit=5&appid=' + apiKey;
     fetch(url)
     .then(function(response) {
       return response.json();
@@ -48,8 +101,9 @@ function getLatLon(search) {
         return;
       }
     })
-
 }
+
+// Function to use the returned latitude and longitude data to search for and retrieve weather data from API
 
 function getCityInfo(cityData) {
 
@@ -64,66 +118,147 @@ function getCityInfo(cityData) {
           return response.json();
         })
         .then(function (data) {
-          populateFiveDayForecast(data, data.timezone);
-          populateCurrentWeather(cityName, data, data.timezone)
+          populateFiveDayForecast(data);
+          populateCurrentWeather(cityName, data)
+
+          console.log(data);
         })
         .catch(function (err) {
           console.error(err);
         });
-    // console.log(url); 
-};
-
-function populateCurrentWeather(city, weather, time) {
-  var date = dayjs.tz(time).format('MMM D, YYYY');
-  var currentDate = document.querySelector('#date1');
-  var temp = document.querySelector('#temp1');
-  var windDirection = document.querySelector('#windDirection1');
-  var windSpeed = document.querySelector('#windSpeed1');
-  var humidity = document.querySelector('#humidity1');
-  currentDate.textContent = date;
-
-  temp.textContent = weather.list[0].main.temp
-  windDirection.textContent = weather.list[0].wind.deg;
-  windSpeed.textContent = weather.list[0].wind.speed;
-  humidity.textContent = weather.list[0].main.humidity
-
   
+}
 
-} 
+  // Uses the data from getCityInfo as parameters to identify city name, our weather data, and timezone
 
-function populateFiveDayForecast(weather, time) {
-
-  for (let i = 1; i < 6; i++) {
-
-    var temp = parseInt
-    var windDirection = document.getElementById('windDirection' + [i]);
-    var windSpeed = document.getElementById('windSpeed' + [i]);
-    var humidity = document.getElementById('humidity' + [i]);
-
-
-    temp.textContent = weather.list[i].main.temp
-    windDirection.textContent = weather.list[i].wind.deg;
-    windSpeed.textContent = weather.list[i].wind.speed;
-    humidity.textContent = weather.list[i].main.humidity
-  }
+function populateCurrentWeather(city, weather) {
+  
+  var currentDate = $('#date1');
+  var temp = $('#temp1');
+  var windDirection = $('#windDirection1');
+  var windSpeed = $('#windSpeed1');
+  var humidity = $('#humidity1');
+  
+  currentDate.text(date);
+  temp.text(weather.list[0].main.temp)
+  windDirection.text(weather.list[0].wind.deg);
+  windSpeed.text(weather.list[0].wind.speed);
+  humidity.text(weather.list[0].main.humidity);
 
 }
 
 
-searchBtn.addEventListener('click', submitHandler);
+// Function to populate the five day forecast cards
 
+function populateFiveDayForecast(weather) {
+ 
+  date = dayjs().format('MMM D, YYYY')
+  // Dates 
+    
+  date2.text(dayjs(date).add(1, 'day').format('MMM D'));
+  date3.text(dayjs(date).add(2, 'day').format('MMM D'));
+  date4.text(dayjs(date).add(3, 'day').format('MMM D'));
+  date5.text(dayjs(date).add(4, 'day').format('MMM D'));
+  date6.text(dayjs(date).add(5, 'day').format('MMM D'));
+  
+  
+  // Temps  
+  
+  temp2.text(weather.list[1].main.temp);
+  temp3.text(weather.list[2].main.temp);
+  temp4.text(weather.list[3].main.temp);
+  temp5.text(weather.list[4].main.temp);
+  temp6.text(weather.list[5].main.temp);
+
+   // Wind direction
+   
+   
+  windDirection2.text(weather.list[1].wind.deg);
+  windDirection3.text(weather.list[2].wind.deg);
+  windDirection4.text(weather.list[3].wind.deg);
+  windDirection5.text(weather.list[4].wind.deg);
+  windDirection6.text(weather.list[5].wind.deg);
+
+   // Wind speed
+
+   
+  windSpeed2.text(weather.list[1].wind.speed);
+  windSpeed3.text(weather.list[2].wind.speed);
+  windSpeed4.text(weather.list[3].wind.speed);
+  windSpeed5.text(weather.list[4].wind.speed);
+  windSpeed6.text(weather.list[5].wind.speed);
+
+  // Humidity
 
   
+  humidity2.text(weather.list[1].main.humidity);
+  humidity3.text(weather.list[2].main.humidity);
+  humidity4.text(weather.list[3].main.humidity);
+  humidity5.text(weather.list[4].main.humidity);
+  humidity6.text(weather.list[5].main.humidity);
+
+  } 
+
+  // Function to move the search bar to the left side of the page to make room for the forecast cards  
+
+function moveNavSection() {
+  
+  $('.jumbotron').removeClass('main');
+  $('.jumbotron').addClass('d-flex');
+  $('#navSection').removeClass();
+  $('#navSection').css('height', '100vh');
+  $('#navSection').addClass('container');
+  $('#nevSection').addClass('col-2')
+  $('#sixDayForecast').removeClass('d-none');
+}
+
+  // Function to clear the results field when searching for a new city from the same page
+
+function clearResultsField() {
+
+  date1.siblings().text('');
+  date2.siblings().text('');
+  date3.siblings().text('');
+  date4.siblings().text('');
+  date5.siblings().text('');
+  date6.siblings().text('');
+
+}
+    
+    
+// Function to create buttons on page representing recent search history
+function renderButtons() {
+
+  
+  
+  $("#recent-search").empty();
+
+
+  for (var i = 0; i < cities.length; i++) {
+
+    var a = $("<button>");
+    
+    a.addClass("city-name");
+    
+    a.attr("data-name", cities[i]);
+    
+    a.text(cities[i]);
+
+    var history = localStorage.getItem("Search Result") || 0;
+    localStorage.setItem("Search Result", cities);
+
+    
+    $("#recent-search").append(a);
+  }
+}
+
+// Creating a clickable button on the search button 
+// Calls retrieveHistory on page load to create history buttons
+
+searchBtn.on('click', submitSearch);
+retrieveHistory();
 
 
 
 
-
-// create a few reusable variables containing main apiurl and apikey
-// Need a function to populate the current day weather card
-// Need a function to fill the five day fprecast cards, for loop 
-// Function to get lat, lon from api
-// Function using the lat and lon for city to make call for weather
-// Function to add recent search to search history and redraw buttons to page
-// Function to add search history buttons to page
-// Function to pull recent searches from local storage and have buttons for them create on page
+    
